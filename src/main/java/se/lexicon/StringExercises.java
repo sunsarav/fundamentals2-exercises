0df2f2c787ef7c7f76f387893fc3dfe85aa1c5e3
Shanmuga Saravanan <sunsarav16@gmail.com>
2026-03-04 17:31:40 +0100

    Added 2 StringExercises

---
+++ src/main/java/se/lexicon/StringExercises.java
@@ -0,0 +1,23 @@
+package se.lexicon;
+
+public class StringExercises {
+    static void main() {
+
+    //Exercise 1 – Length of a String
+
+        String name = "Java";
+        int length = name.length();
+        System.out.println("The Length of Java is: " + length );
+
+    //Exercise 2 – Character at Index
+
+        String text = "Long example sentence";
+        char result = text.charAt(6);
+        System.out.println("The character at 6th place is: " + result);
+
+    //Exercise 3 – Index Position of 'o'
+
+
+
+    }
+}
